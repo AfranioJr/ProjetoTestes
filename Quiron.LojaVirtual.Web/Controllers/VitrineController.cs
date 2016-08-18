c3f597e4f17b34125d19c7147c95fb996c651362
Sirilo <junior.asj@gmail.com>
2016-08-18 15:40:52 -0300

    Vitrine + Paginação

---
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        private ProdutosRespositorios _repositorio;
+        //Quantidade de produtos na página
+        public int ProdutosPorPagina = 10;
+
+        public ActionResult ListaProdutos(int pagina = 1)
+        {
+            _repositorio = new ProdutosRespositorios();
+            var produtos = _repositorio.Produtos
+                .OrderBy(p => p.Descricao)
+                .Skip((pagina - 1) * ProdutosPorPagina)
+                .Take(ProdutosPorPagina);
+
+            return View(produtos);
+        }
+	}
+}
